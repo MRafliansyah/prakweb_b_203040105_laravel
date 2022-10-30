ec82562b0a1b5174897af6210e74afdc8a134c88
MRafliansyah <203040105@mail.unpas.ac.id>
2022-10-30 15:10:39 +0700

    Video10 factory & faker setiap push aga sedkit bermasalah

--- prakweb_b_203040105_laravel/database/seeders/DatabaseSeeder.php
+++ prakweb_b_203040105_laravel/database/seeders/DatabaseSeeder.php
@@ -20,19 +20,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        
 
-        User::create([
-        'name' => 'Rafliansyah',
-        'email' => 'rafliansyah11@gmail.com',
-        'password' => bcrypt("12345")
-        ]);
+        // User::create([
+        // 'name' => 'Rafliansyah',
+        // 'email' => 'rafliansyah11@gmail.com',
+        // 'password' => bcrypt("12345")
+        // ]);
 
-        User::create([
-        'name' => 'Raihan har',
-        'email' => 'raihannhar@gmail.com',
-        'password' => bcrypt("12345")
-        ]);
+        // User::create([
+        // 'name' => 'Raihan har',
+        // 'email' => 'raihannhar@gmail.com',
+        // 'password' => bcrypt("12345")
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -43,40 +45,41 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
        
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul pertama',
-            'excerpt' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur.',
-            'body' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur, velit numquam dignissimos placeat aperiam repellendus explicabo. Ipsam, quisquam sit magni dolor ab nobis repellendus repellat non, distinctio itaque illo suscipit labore quam voluptas? Possimus et cupiditate sit optio consectetur. Molestiae minima et velit eum labore! Veniam nihil obcaecati velit illo odio reiciendis dolores pariatur quo laudantium recusandae inventore architecto vero aliquid expedita, autem cumque sint ea fugiat porro molestiae perferendis dolorem exercitationem dignissimos.',
-            'category_id' => 1,
-            'user_id'=>1
-        ]);
-        Post::create([
-            'title' => 'Judul Kedua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur.',
-            'body' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur, velit numquam dignissimos placeat aperiam repellendus explicabo. Ipsam, quisquam sit magni dolor ab nobis repellendus repellat non, distinctio itaque illo suscipit labore quam voluptas? Possimus et cupiditate sit optio consectetur. Molestiae minima et velit eum labore! Veniam nihil obcaecati velit illo odio reiciendis dolores pariatur quo laudantium recusandae inventore architecto vero aliquid expedita, autem cumque sint ea fugiat porro molestiae perferendis dolorem exercitationem dignissimos.',
-            'category_id' => 1,
-            'user_id'=>1
-        ]);
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur.',
-            'body' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur, velit numquam dignissimos placeat aperiam repellendus explicabo. Ipsam, quisquam sit magni dolor ab nobis repellendus repellat non, distinctio itaque illo suscipit labore quam voluptas? Possimus et cupiditate sit optio consectetur. Molestiae minima et velit eum labore! Veniam nihil obcaecati velit illo odio reiciendis dolores pariatur quo laudantium recusandae inventore architecto vero aliquid expedita, autem cumque sint ea fugiat porro molestiae perferendis dolorem exercitationem dignissimos.',
-            'category_id' => 2,
-            'user_id'=>1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul pertama',
+        //     'excerpt' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur.',
+        //     'body' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur, velit numquam dignissimos placeat aperiam repellendus explicabo. Ipsam, quisquam sit magni dolor ab nobis repellendus repellat non, distinctio itaque illo suscipit labore quam voluptas? Possimus et cupiditate sit optio consectetur. Molestiae minima et velit eum labore! Veniam nihil obcaecati velit illo odio reiciendis dolores pariatur quo laudantium recusandae inventore architecto vero aliquid expedita, autem cumque sint ea fugiat porro molestiae perferendis dolorem exercitationem dignissimos.',
+        //     'category_id' => 1,
+        //     'user_id'=>1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur.',
+        //     'body' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur, velit numquam dignissimos placeat aperiam repellendus explicabo. Ipsam, quisquam sit magni dolor ab nobis repellendus repellat non, distinctio itaque illo suscipit labore quam voluptas? Possimus et cupiditate sit optio consectetur. Molestiae minima et velit eum labore! Veniam nihil obcaecati velit illo odio reiciendis dolores pariatur quo laudantium recusandae inventore architecto vero aliquid expedita, autem cumque sint ea fugiat porro molestiae perferendis dolorem exercitationem dignissimos.',
+        //     'category_id' => 1,
+        //     'user_id'=>1
+        // ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur.',
+        //     'body' => 'fugiat itaque sint voluptatem id expedita mollitia pariatur, velit numquam dignissimos placeat aperiam repellendus explicabo. Ipsam, quisquam sit magni dolor ab nobis repellendus repellat non, distinctio itaque illo suscipit labore quam voluptas? Possimus et cupiditate sit optio consectetur. Molestiae minima et velit eum labore! Veniam nihil obcaecati velit illo odio reiciendis dolores pariatur quo laudantium recusandae inventore architecto vero aliquid expedita, autem cumque sint ea fugiat porro molestiae perferendis dolorem exercitationem dignissimos.',
+        //     'category_id' => 2,
+        //     'user_id'=>1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Keempat',
-            'slug' => 'judul-keempat',
-            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla,',
-            'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> <p>Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!</p><p>Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!</p>',
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Keempat',
+        //     'slug' => 'judul-keempat',
+        //     'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla,',
+        //     'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> <p>Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!</p><p>Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cupiditate praesentium facere explicabo, maiores architecto adipisci reprehenderit nulla, consectetur ex magni labore nesciunt veniam exercitationem aliquid mollitia error, natus nam!</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+        // ]);
         
         // \App\Models\User::factory()->create([
         
--- prakweb_b_203040105_laravel/routes/web.php
+++ prakweb_b_203040105_laravel/routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
-
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ Route::get('/', function () {
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
-        "name" => "MuahamadRafliansyah",
+        "name" => "MuhamadRafliansyah",
         "email" => "203040105@unpas.mail.ac.id",
         "image" => "rf.jpeg"
 
@@ -50,6 +50,13 @@ Route :: get('/categories/{category:slug}', function(Category $category){
     return view('category', [
         'title' => $category->name,
         'posts' => $category->posts,
-        'category' =>$category->name
+        'category' => $category->name
     ]);
+});
+
+Route::get('/authors/{author:username}', function(User $author) {
+return view('posts', [
+    'title' => 'User Post',
+    'posts' => $author->posts
+]);
 });
