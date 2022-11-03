e0de97afee89fc93b26b759bdaee7c407551d5ff
MRafliansyah <203040105@mail.unpas.ac.id>
2022-11-03 23:20:58 +0700

    Video 16 User Login & Middleware

--- prakweb_b_203040105_laravel/routes/web.php
+++ prakweb_b_203040105_laravel/routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
@@ -67,7 +68,10 @@ return view('posts', [
 ]);
 });
 
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/login', [LoginController::class, 'index']);
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
