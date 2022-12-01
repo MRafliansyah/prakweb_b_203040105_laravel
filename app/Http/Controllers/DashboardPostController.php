4bc9700dec52b0e21bfde53b73622ba3f14ee006
MRafliansyah <203040105@mail.unpas.ac.id>
2022-12-02 02:25:34 +0700

    update, preview,update&delete image

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,7 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
-
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -105,6 +105,7 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
 
@@ -114,6 +115,13 @@ class DashboardPostController extends Controller
         }
 
         $validatedData = $request->validate($rules);
+
+        if($request->file('image')) {
+            if($request->oldImage) {
+                Storage::delete([$request->oldImage]);
+            }
+            $validateData['image'] = $request->file('image')->store('post-images');
+        }
 
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
@@ -133,6 +141,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if($post->image) {
+            Storage::delete([$post->image]);
+        }
 
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'post has been deleted');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.main')
 
 @section('container')
-<h1>Halaman Home</h1>
+{{-- <!DOCTYPE html>
+ <html>
+ <head>
+  <title>Atribut Width dan Height di Tag Marquee</title>
+ </head>
+ <body> --}}
+    <h1>Halaman Home</h1>
+  <marquee width="300" height="40">SELAMAT DATANG DI BLOG RF</marquee>
+ {{-- </body>
+ </html> --}}
 @endsection
+
